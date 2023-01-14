44cf4610ef4c5361eb4bb0b95c89d7d9716c9ae4
MoonShine <ljhmyw@foxmail.com>
2023-01-14 21:25:48 +0800

    Initial commit

---
+++ Harmony/PatchMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace AK_Industry
+{
+    [StaticConstructorOnStartup]
+    public class PatchMain
+    {
+        static PatchMain()
+        {
+            var instance = new Harmony("AK_Industry");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AK_Industry
+ 舟mod本体的附属mod - 源石工业系统 的功能dll
