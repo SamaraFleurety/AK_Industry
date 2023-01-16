184a4c7f82cc1c078718feafeb26101f603a18d8
MoonShine <ljhmyw@foxmail.com>
2023-01-17 01:39:59 +0800

    把框架里面的气体系统移过来

---
+++ GasSystem/Thing/Def/GasDef.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace AK_Industry
+{
+	public class GasDef : ThingDef
+	{
+		public HediffDef addHediff;
+
+		public float hediffAddChance = 1f;
+
+		public float hediffSeverity = 0.05f;
+
+		public int interval = 1;
+
+		public bool ignoreAnimals;
+
+		public bool ignoreNormalFlesh;
+
+		public bool ignoreInsectFlesh;
+
+		public bool ignoreMechanoidFlesh;
+
+		//public bool ignoreToxicSensitivity;
+
+		public StatDef resistedBy = StatDefOf.ToxicResistance;
+	}
+}
